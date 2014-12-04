28b5f08a85b5aa9f669b04e41ead093ae82b3945
elyselturner <elyse.turner@detroitlabs.com>
2014-12-04 17:35:55 -0500

    I added dummy pages for the exercise page

---
+++ app/src/main/java/fragments/ExerciseChoicesFragment.java
@@ -0,0 +1,9 @@
+package fragments;
+
+/**
+ * Created by elyseturner on 12/4/14.
+ */
+public class ExerciseChoicesFragment {
+    //this fragment is going to present the user with 6 choices of workouts then send the choices over to the API call/ parsing noise.
+
+}
--- app/src/main/java/fragments/WhatWorkoutDitIDoFragment.java
+++
@@ -1,7 +0,0 @@
-package fragments;
-
-/**
- * Created by elyseturner on 12/4/14.
- */
-public class WhatWorkoutDitIDoFragment {
-}
