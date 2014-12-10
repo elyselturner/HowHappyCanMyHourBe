90ae3e7ce44b38d57cc65843d74d8bff7dfc556d
elyselturner <elyse.turner@detroitlabs.com>
2014-12-10 16:41:17 -0500

    I added a third spinner for the drink choices

--- app/src/main/java/com/example/elyseturner/howhappycanmyhourbe/activities/fragments/ExerciseFragment.java
+++ app/src/main/java/com/example/elyseturner/howhappycanmyhourbe/activities/fragments/ExerciseFragment.java
@@ -5,13 +5,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.elyseturner.howhappycanmyhourbe.R;
 
 /**
  * Created by elyseturner on 12/9/14.
  */
-public class ExerciseFragment extends Fragment {
+public class ExerciseFragment extends Fragment implements AdapterView.OnItemSelectedListener{
 
     public ExerciseFragment() {
     }
@@ -20,8 +23,33 @@ public class ExerciseFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_exercise, container, false);
+
+
+        Spinner spinner = (Spinner) rootView.findViewById(R.id.exercise_choice);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.exercise_choices, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(this);
+
+
         return rootView;
     }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
 }
 
 
