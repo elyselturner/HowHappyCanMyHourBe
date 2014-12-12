81fd0dd6bed51123b243a0027bdb9ae8a7bd7ca0
elyselturner <elyse.turner@detroitlabs.com>
2014-12-12 11:43:13 -0500

    I created the exercise model

---
+++ app/src/main/java/com/example/elyseturner/howhappycanmyhourbe/activities/models/DrinkModel.java
@@ -0,0 +1,7 @@
+package com.example.elyseturner.howhappycanmyhourbe.activities.models;
+
+/**
+ * Created by elyseturner on 12/12/14.
+ */
+public class DrinkModel {
+}
--- app/src/main/java/com/example/elyseturner/howhappycanmyhourbe/activities/models/ExerciseModel.java
+++ app/src/main/java/com/example/elyseturner/howhappycanmyhourbe/activities/models/ExerciseModel.java
@@ -4,4 +4,37 @@ package com.example.elyseturner.howhappycanmyhourbe.activities.models;
  * Created by elyseturner on 12/12/14.
  */
 public class ExerciseModel {
+    private String name;
+    private String type;
+    private double calories;
+
+    public ExerciseModel(String name, String type, double calories) {
+        this.name = name;
+        this.type = type;
+        this.calories = calories;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getCalories() {
+        return calories;
+    }
+
+    public void setCalories(double calories) {
+        this.calories = calories;
+    }
 }
