6b6931761592d804666074abd577ad77f1cc24bf
elyselturner <elyse.turner@detroitlabs.com>
2014-12-30 15:17:22 -0500

    Making the fab invisible until after the third spinner populates

--- app/src/main/java/com/example/elyseturner/howhappycanmyhourbe/activities/fragments/ExerciseTimeDrinkFragment.java
+++ app/src/main/java/com/example/elyseturner/howhappycanmyhourbe/activities/fragments/ExerciseTimeDrinkFragment.java
@@ -1,13 +1,11 @@
 package com.example.elyseturner.howhappycanmyhourbe.activities.fragments;
 
 import android.app.Fragment;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewOutlineProvider;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
@@ -22,6 +20,7 @@ import com.example.elyseturner.howhappycanmyhourbe.activities.parsers.DrinkParse
 import com.example.elyseturner.howhappycanmyhourbe.activities.parsers.ExerciseParser;
 import com.example.elyseturner.howhappycanmyhourbe.activities.requests.DrinkApiRequest;
 import com.example.elyseturner.howhappycanmyhourbe.activities.requests.ExerciseApiRequest;
+import com.melnykov.fab.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,33 +28,21 @@ import java.util.List;
 /**
  * Created by elyseturner on 12/9/14.
  */
-public class ExerciseTimeDrinkFragment extends Fragment implements AdapterView.OnItemSelectedListener{
+public class ExerciseTimeDrinkFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_exercise_time_drink, container, false);
         return rootView;
 
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            ViewOutlineProvider newOutline = ViewOutlineProvider.BACKGROUND;
-
-            //this is setting the image button with id "fab" to have a new view outline provider
-            rootView.findViewById(R.id.fab).setOutlineProvider(newOutline);
-        }
-
-//        int size = getResources().getDimensionPixelSize(R.dimen.fab_size);
-//        ViewOutlineProvider outline = new ViewOutlineProvider() {
-//
-//        ViewOutlineProvider.setRectangle(0, 0, size, size);
-//        rootView.findViewById(R.id.fab).setOutlineProvider(new Outline);
     }
-
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
@@ -64,22 +51,24 @@ public class ExerciseTimeDrinkFragment extends Fragment implements AdapterView.O
         addTimeToSpinner(view);
         addDrinkToSpinner(view);
 
+
     }
 
-    public void  addExerciseToSpinner(View rootView){
+    public void addExerciseToSpinner(View rootView) {
 
         ArrayList<ExerciseModel> theInfoYouWanted = new ArrayList<ExerciseModel>();
 
-        final ExerciseAdapter adapter = new ExerciseAdapter (getActivity(), R.layout.custom_spinner_exercise_row, theInfoYouWanted);
+        final ExerciseAdapter adapter = new ExerciseAdapter(getActivity(), R.layout.custom_spinner_exercise_row, theInfoYouWanted);
 
 
         new ExerciseApiRequest(new ApiCallBack() {
             @Override
-            public void onSuccess(String resultsString)  {
+            public void onSuccess(String resultsString) {
 
                 List<ExerciseModel> theInfoYouWanted = new ExerciseParser().parsePostingFromJsonString(resultsString);
-                Log.d(ExerciseTimeDrinkFragment.class.getName(),theInfoYouWanted.toString());
+                Log.d(ExerciseTimeDrinkFragment.class.getName(), theInfoYouWanted.toString());
                 adapter.addAll(theInfoYouWanted);
+                
             }
 
             @Override
@@ -90,64 +79,68 @@ public class ExerciseTimeDrinkFragment extends Fragment implements AdapterView.O
 
         Spinner spinnerExercise = (Spinner) rootView.findViewById(R.id.exercise_choice);
 
-        // Apply the adapter to the spinner
-        spinnerExercise.setAdapter(adapter);
 
+        spinnerExercise.setAdapter(adapter);
 
 
         spinnerExercise.setOnItemSelectedListener(this);
     }
 
 
-    public void  addTimeToSpinner(View rootView){
+    public void addTimeToSpinner(View rootView) {
 
         Spinner spinnerTime = (Spinner) rootView.findViewById(R.id.time_choice);
-        // Create an ArrayAdapter using the string array and a default spinner layout
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.time_choices, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
+
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
+
         spinnerTime.setAdapter(adapter);
 
         spinnerTime.setOnItemSelectedListener(this);
     }
 
-    public void  addDrinkToSpinner(View rootView){
-
+    public void addDrinkToSpinner(final View rootView) {
         ArrayList<DrinkModel> theInfoYouWanted = new ArrayList<DrinkModel>();
 
-        final DrinkAdapter adapter = new DrinkAdapter(getActivity(), R.layout.custom_spinner_drink_row, theInfoYouWanted );
+        final DrinkAdapter adapter = new DrinkAdapter(getActivity(), R.layout.custom_spinner_drink_row, theInfoYouWanted);
 
 
         new DrinkApiRequest(new ApiCallBack() {
             @Override
-            public void onSuccess(String resultsString)  {
+            public void onSuccess(String resultsString) {
 
                 List<DrinkModel> theInfoYouWanted = new DrinkParser().parsePostingFromJsonString(resultsString);
-                Log.d(ExerciseTimeDrinkFragment.class.getName(),theInfoYouWanted.toString());
+                Log.d(ExerciseTimeDrinkFragment.class.getName(), theInfoYouWanted.toString());
                 adapter.addAll(theInfoYouWanted);
+
+//                FloatingActionButton fab = (FloatingActionButton) rootView.findViewById(R.id.fab);
+//                fab.setVisibility(View.VISIBLE);
+
             }
 
             @Override
             public void onFailure() {
+
 
             }
         }).execute();
 
         Spinner spinnerDrink = (Spinner) rootView.findViewById(R.id.drink_choice);
 
-        // Apply the adapter to the spinner
         spinnerDrink.setAdapter(adapter);
-
-
 
         spinnerDrink.setOnItemSelectedListener(this);
     }
 
 
     @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+    public void onItemSelected(AdapterView<?> parent, final View view, int position, long id) {
         String itemSelectedInSpinner = parent.getItemAtPosition(position).toString();
+
+
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        fab.setVisibility(View.VISIBLE);
 
     }
 
